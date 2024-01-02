73b1c4422b16c4169ac0785e332a1f96fde48586
Derian Molina Lopez <derianmolina220@gmail.com>
2024-01-02 10:56:06 -0700

    terminando con la primera pagina de las tablas

---
+++ CRMreact/src/components/Cliente.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Cliente = ({ Cliente }) => {
+    const { nombre, empresa, email } = Cliente
+    return (
+      
+            <tr className='border-b'>
+                <td className='p-6 space-y-2'>
+                    <p className='text-2xl text-gray-800'>
+                        {nombre}
+                    </p>
+                    <p>
+                        {empresa}
+                    </p>
+                </td>
+                <td className='p-6 space-y-2'>
+                    <p className='text'>
+                        <span className='text-gray-800 uppercase font-bold'>
+                            email
+                        </span>
+                        {email}
+                    </p>
+                </td>
+                <td>
+                    <button type='button'
+                        className='text-blue-600 hover:text-blue-700 uppercase font-bold text-xs'
+                    >
+                        Modificar
+
+                    </button>
+                    <button type='button'
+                        className='text-red-600 hover:text-red-700 uppercase font-bold text-xs'
+                    >
+                        eliminar
+
+                    </button>
+                </td>
+            </tr>
+        
+    )
+}
+
+export default Cliente
--- CRMreact/src/pages/Index.jsx
+++ CRMreact/src/pages/Index.jsx
@@ -1,14 +1,91 @@
 import React from 'react'
+import {useLoaderData} from 'react-router-dom'
+import Cliente from '../components/Cliente';
+//*el hecho de usar un loader, es simulando un efecto cargando la informacion de golpe
 export function loader(){
+
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+
+
     //una funcion que simula hacer una carga en el cambio de pagina para que pueda
     //darle tiempo a la hora de hacer un efecto auxiliar
+    //!esta fallando obviamente por que aun no especifico que es lo que va a regresar
+    return clientes
 }
 
 const Index = () => {
+
+  const datos = useLoaderData()
+  console.log(datos)
+  /*
+  !el useLoaderData es una funcion que esta asociada
+  !por por el routin, ahi se define el loader, y ese loader 
+  !realiza la operacion que tenemos en la parte superior 
+  */
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
       <p className='mt-10'>Administra a tus clientes</p>
+      {datos.length ? (
+         <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-blue-800 text-white'>
+            <tr>
+              <th className='p-2'>Cliente</th>
+              <th className='p-2'>Cliente</th>
+              <th className='p-2'>Cliente</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {datos.map(cliente=>{
+              return (
+                <Cliente Cliente={cliente} key={cliente.id}></Cliente>
+              )
+              
+               
+            })}
+          </tbody>
+
+         </table>
+      ) : (
+        <p className='mt-5 text-center text-2xl'>No hay clientes</p>
+      )}
     </>
   )
 }

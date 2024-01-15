d7b4b1d5f30e55e84255e90ccf293b86eb44332a
Derian Molina Lopez <derianmolina220@gmail.com>
2024-01-15 12:49:01 -0700

    problema con la url de la api

--- clima/src/components/Formulario.jsx
+++ clima/src/components/Formulario.jsx
@@ -1,29 +1,51 @@
 import React from 'react'
+import { useEffect , useState} from 'react'
+import useClima from '../hooks/useClima'
 
 const Formulario = () => {
+   const {busqueda,setBusqueda,consultarClima,datosBusqueda} = useClima()
+   const [alerta, setAlerta] = useState(false)
+   const {ciudad,pais} = busqueda
+
+
+   const handleChange = e => {
+    e.preventDefault()
+       setBusqueda({
+           ...busqueda,
+           [e.target.name]: e.target.value
+       });
+   };
+  const handleSubmit = e=>{
+  e.preventDefault()
+  if(ciudad.trim() === "" || pais === ""){
+    console.log("hay campos vacios")
+    setAlerta(true)
+    return
+  }
+  //aqui consulto el clima
+  console.log(busqueda.ciudad)
+  consultarClima(busqueda)
+}
+
   return (
     <div className='contenedor'>
-        <form action="">
+      { alerta && <p>Hay al menos un campo vacio</p>}
+        <form onSubmit={handleSubmit} action="">
           <div className='campo'>
             <label htmlFor="ciudad">Ciudad</label>
-            <input type="text" name="ciudad" id="ciudad" />
-
+            <input type="text" name="ciudad" id="ciudad" onChange={handleChange} value={ciudad} />
           </div>
           <div className='campo'>
             <label htmlFor="Pais">Pais</label>
-            
-            <select name="pais" id="pais">
+            <select name="pais" id="pais" value={pais} onChange={handleChange}>
               <option value="">--Selecciona un pais--</option>
               <option value="US">Estados unidos</option>
               <option value="CO">Colombia</option>
               <option value="ES">España</option>
-              
-              
             </select>
           </div>
           <input type="submit" value="Consultar el clima" id="" />
         </form>
-      
     </div>
   )
 }
--- clima/src/context/ClimaProvider.jsx
+++ clima/src/context/ClimaProvider.jsx
@@ -1,12 +1,37 @@
 import { useState,createContext } from "react";
 
 const ClimaContext = createContext();
-
+import axios from 'axios'
 const ClimaProvider = ({children}) => {
+
     const [busqueda,setBusqueda] = useState({
         cidad:'',
         pais:''
     })
+    const consultarClima = async datos=>{
+        console.log(datos)
+        console.log(import.meta.env.VITE_API_LLAVE)
+        try{
+            const {ciudad,pais} = datos
+            const appID = import.meta.env.VITE_API_LLAVE
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}, ${pais}&limit=1&appid=${appID}`
+            const {data} = await axios(url)
+            const {lat,lon} = data[0]
+           //extraer el lat y lon
+           
+           console.log(lat)
+           console.log(lon) 
+           console.log(data)
+            //console.log(data.lon)
+           const urlLan = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${appID}`
+           console.log(urlLan)
+             const {data:clima} = await axios(urlLan)
+            console.log(clima)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     const datosBusqueda = (e) => {
         setBusqueda({
             ...busqueda,
@@ -17,7 +42,10 @@ const ClimaProvider = ({children}) => {
         <ClimaContext.Provider
            value={{ //!ccuidao con la ortografia, si te equivocas escriviendo el valor, no te dice si esta mal, solo te da na advertencia
             ClimaProvider,
-            datosBusqueda
+            datosBusqueda,
+            busqueda,
+            setBusqueda,
+            consultarClima
            }}
         >
             {children}

ee09b5702b42563658e67d2ccc29eb5f3699f9e6
Derian Molina Lopez <derianmolina220@gmail.com>
2024-02-05 16:26:41 -0700

    nuevo endpoint agregado, para la modificacion del campo de estado

--- comidaquiosco/pages/admin.js
+++ comidaquiosco/pages/admin.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useSWR from 'swr'
 import axios from 'axios'
+import Orden from '../components/Orden'
 import AdminLayout from '../layout/AdminLayout'
 const admin = () => {
 
@@ -11,6 +12,16 @@ const admin = () => {
     <AdminLayout pagina="admin">
       <h1> Panel de administracion</h1>
       <p>Gestiona tus ordenes</p>
+    {
+  data && data.length > 0 
+    ? data.map(orden => (
+        <Orden 
+          key={orden.id}
+          orden={orden}
+        />
+      ))
+    : <p>No hay ordenes pendientes</p>
+}
     </AdminLayout>
   )
 }
